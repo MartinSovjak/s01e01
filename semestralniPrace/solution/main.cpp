6b7b790994dd0e301f8095f8530dce5bbd670cda
MartinSovjak <32803336+MartinSovjak@users.noreply.github.com>
2017-12-03 10:44:22 +0100

    Update main.cpp

--- semestralniPrace/solution/main.cpp
+++ semestralniPrace/solution/main.cpp
@@ -9,7 +9,8 @@
 #include <stdlib.h>
 #include <ctype.h>
 using namespace std;
-
+#define OKAY        0
+#define IO_ERROR    1
 
 struct Mistnosti{
 
@@ -21,7 +22,7 @@ struct Mistnosti{
 };
 struct Rezervace {
         int id;
-        string denRezervace;
+        int datum;
 
 };
 
@@ -60,16 +61,82 @@ void nactiMistnosti (ifstream &soubor,Mistnosti mistnosti[]){
     }
 
 }
-int vypisRezervace(Rezervace rezervace[],int pocetRezervace){
+void nactiRezervace (ifstream &soubor, Rezervace rezervace[]){
+    // OSETRIT I/O EXCEPTIONS
+    soubor.clear();
+    soubor.seekg(0, ios::beg);
+    string radek,token;
+    char ch;
+    int i = 0;
+    while(getline(soubor,radek)){
+        istringstream ss(radek);
+        ss >> rezervace[i].id;
+        ss >> ch;
+        ss >> rezervace[i].datum;
+        i++;
+
+    }
+}
+int startHTML(const char* htmlSoubor){
+
+    ofstream myFile;
+    myFile.exceptions ( ofstream::failbit | ofstream::badbit );
+        try {
+            myFile.open (htmlSoubor, std::ofstream::out);
+            myFile << "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">" << endl; //starting html
+            myFile << "<html>" << endl << "<head>" << endl << "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">" << endl << "<title>C++</title>" << endl << "</head>" << endl;
+            myFile << "<body>" << endl;
+            myFile << "<table width=\"900\">" << endl << "<tr><th width=\"180\">ID</th><th width=\"180\">PATRO</th><th width=\"180\">CISLO MISTNOSTI</th><th width=\"180\">KAPACITA</th><th width=\"180\">CENA ZA DEN</th></tr>" << endl;
+        }
+        catch (const ofstream::failure& e){
+            return (IO_ERROR);
+        }
+    myFile.close();
+
+return(OKAY);
+
+}
+int endHTML(const char* htmlSoubor){
+    ofstream myFile;
+    myFile.exceptions ( ofstream::failbit | ofstream::badbit );
+     try {
+        myFile.open (htmlSoubor, std::ofstream::out | std::ofstream::app);
+
+        myFile << "</table>" << endl;
+        myFile << "</body>" << endl;
+        myFile << "</html>" << endl;
+     }
+     catch(const ofstream::failure& e){
+        return(IO_ERROR);
+     }
+     myFile.close();
+
+
+return (OKAY);
+
+}
+int zapisDoHTMLPrvnichX(Mistnosti mistnosti[], int pocetCiselMensich, const char* htmlSoubor){
+
+startHTML(htmlSoubor);
+for (int i = 0; i < pocetCiselMensich; i++){
+        cout << mistnosti[i].id << "|" << mistnosti[i].patro << "|" << mistnosti[i].jmenoMistnosti << "|" << mistnosti[i].kapacitaSedadel << "|" << mistnosti[i].cenaDen << endl;
+    }
+endHTML(htmlSoubor);
+return (0);
+}
+int zapisDoHTML(Mistnosti mistnosti[], int pocetMistnosti,const char* htmlSoubor){
+
+return (0);
+}
+int vypisRezervace(Rezervace rezervace[],int pocetRezervace,const char* htmlSoubor){
 
 return(0);
 
 }
-int vypisPodleMist(Mistnosti mistnosti[],int pocetMistnosti){
+int vypisPodleMist(Mistnosti mistnosti[],int pocetMistnosti,const char* htmlSoubor){
     cout << "Jak chcete mistnosti vypsat?" << endl;
     cout << "Od nejmensiho poctu mist - 1" << endl;
     cout << "Od nejvyssiho poctu mist - 2" << endl;
-    int error = 0;
     string cislicko;
     int cisloAkce;
 
@@ -78,13 +145,13 @@ int vypisPodleMist(Mistnosti mistnosti[],int pocetMistnosti){
     if (cislicko.length() == 1){
         if (cislicko.at(0) < '0' || cislicko.at(0) > '9'){
             cout << "Spatne zadane cislo,zkuste to znovu." << endl << endl;
-            return (vypisPodleMist(mistnosti,pocetMistnosti));
+            return (vypisPodleMist(mistnosti,pocetMistnosti,htmlSoubor));
         }
         cisloAkce = cislicko.at(0)-'0';
     }
     else {
         cout << "Spatne zadane cislo,zkuste to znovu." << endl << endl;
-        return (vypisPodleMist(mistnosti,pocetMistnosti));
+        return (vypisPodleMist(mistnosti,pocetMistnosti,htmlSoubor));
     }
     int vetsiMensi = 0;
 
@@ -99,14 +166,14 @@ int vypisPodleMist(Mistnosti mistnosti[],int pocetMistnosti){
         }
         default:{
             cout << "Spatne zadane cislo,zkuste to znovu." << endl << endl;
-            return (vypisPodleMist(mistnosti,pocetMistnosti));
+            return (vypisPodleMist(mistnosti,pocetMistnosti,htmlSoubor));
             break;
         }
      }
 
     int i, j;
     struct Mistnosti temp;
-if (vetsiMensi = 1){
+if (vetsiMensi == 1){
     for (i = 0; i < (pocetMistnosti-1);i++){
         for (j = 0; j < (pocetMistnosti-1-i);j++){
             if (mistnosti[j].kapacitaSedadel > mistnosti[j+1].kapacitaSedadel){
@@ -119,7 +186,7 @@ if (vetsiMensi = 1){
 
     }
 }
-if (vetsiMensi = 2){
+if (vetsiMensi == 2){
     for (i = 0; i < (pocetMistnosti-1);i++){
         for (j = 0; j < (pocetMistnosti-1-i);j++){
             if (mistnosti[j].kapacitaSedadel < mistnosti[j+1].kapacitaSedadel){
@@ -133,6 +200,8 @@ if (vetsiMensi = 2){
     }
 
 }
+zapisDoHTML(mistnosti,pocetMistnosti,htmlSoubor);
+
     for (i = 0; i < pocetMistnosti; i++){
         cout << mistnosti[i].id << "|" << mistnosti[i].patro << "|" << mistnosti[i].jmenoMistnosti << "|" << mistnosti[i].kapacitaSedadel << "|" << mistnosti[i].cenaDen << endl;
     }
@@ -140,25 +209,78 @@ if (vetsiMensi = 2){
 return(0);
 
 }
-int vypisPodleCeny(Mistnosti mistnosti[], int pocetMistnosti){
+int vypisPodleCeny(Mistnosti mistnosti[], int pocetMistnosti,const char* htmlSoubor){
+
+    cout << "Jaka ma byt maximalni cena?" << endl;
+    string cislicko;
+    int cisloAkce = 0;
+    int pocetCiselMensich = 0;
+
+    getline(cin,cislicko);
+    int length = cislicko.length();
+    for (int i = 0; i < length; i++){
+        if (cislicko.at(i) < '0' || cislicko.at(i) > '9'){
+
+            cout << "Spatne zadana cena, zkuste to znovu." << endl << endl;
+            return (vypisPodleCeny(mistnosti,pocetMistnosti,htmlSoubor));
+        }
+    }
+    if (length < 1){
+            cout << "Nebylo zadano cislo, zkuste to znovu." << endl << endl;
+            return (vypisPodleCeny(mistnosti,pocetMistnosti,htmlSoubor));
+    }
+        for (int j = 0; j < length;j++){
+            cisloAkce += (cislicko.at(j)-'0') * pow(10,length-1-j);
+
+        }
+
+
+
+    int i, j;
+    struct Mistnosti temp;
+
+    for (i = 0; i < (pocetMistnosti-1);i++){
+        for (j = 0; j < (pocetMistnosti-1-i);j++){
+            if (mistnosti[j].cenaDen > mistnosti[j+1].cenaDen){
+                temp = mistnosti[j];
+                mistnosti[j] = mistnosti[j+1];
+                mistnosti[j+1] = temp;
+            }
+
+        }
+
+    }
+
+    for (i = 0; i < (pocetMistnosti);i++){
+
+            if (mistnosti[i].cenaDen <= cisloAkce){
+                pocetCiselMensich++;
+            }
+
+    }
+zapisDoHTMLPrvnichX(mistnosti,pocetCiselMensich,htmlSoubor);
+
+
+
+
 
 return(0);
 
 }
-int vypisVolneMistnosti(Mistnosti mistnosti[],Rezervace rezervace[],int pocetMistnosti){
+int vypisVolneMistnosti(Mistnosti mistnosti[],Rezervace rezervace[],int pocetMistnosti, const char* htmlSoubor){
 
 return(0);
 
 }
-int rezervaceMista(ifstream &souborRezervace,Mistnosti mistnosti[], Rezervace rezervace[]){
+int rezervaceMista(ifstream &souborRezervace,Mistnosti mistnosti[], Rezervace rezervace[],const char* htmlSoubor){
 
 return(0);
 
 }
-int askForAction(ifstream &souborRezervace, Mistnosti mistnosti[], Rezervace rezervace[], int pocetMistnosti, int pocetRezervace){
+int askForAction(ifstream &souborRezervace, Mistnosti mistnosti[], Rezervace rezervace[], int pocetMistnosti, int pocetRezervace,const char* htmlSoubor){
  cout << "Zadejte cislo akce, kterou chcete provest" << endl;
  cout << "Rezervace mistnosti - 1" << endl;
- cout << "Vypis mistnosti podle jmena - 2" << endl;
+ cout << "Vypis volnych mistnosti na dane datum - 2" << endl;
  cout << "Vypis mistnosti podle ceny - 3" << endl;
  cout << "Vypis mistnosti podle poctu mist - 4" << endl;
  cout << "Vypis rezervovanych mistnosti - 5" << endl << endl;
@@ -170,33 +292,33 @@ getline(cin,cislicko);
 
 if (cislicko.length() == 1){
     if (cislicko.at(0) < '0' || cislicko.at(0) > '9')
-        return (askForAction(souborRezervace,mistnosti,rezervace,pocetMistnosti, pocetRezervace));
+        return (askForAction(souborRezervace,mistnosti,rezervace,pocetMistnosti, pocetRezervace, htmlSoubor));
     cisloAkce = cislicko.at(0)-'0';
 }
 else {
-    return (askForAction(souborRezervace,mistnosti,rezervace,pocetMistnosti, pocetRezervace));
+    return (askForAction(souborRezervace,mistnosti,rezervace,pocetMistnosti, pocetRezervace,htmlSoubor));
 }
 
 
  switch(cisloAkce){
     case 1:{
-        rezervaceMista(souborRezervace,mistnosti,rezervace);
+        rezervaceMista(souborRezervace,mistnosti,rezervace,htmlSoubor);
         break;
     }
     case 2:{
-        vypisVolneMistnosti(mistnosti,rezervace,pocetMistnosti);
+        vypisVolneMistnosti(mistnosti,rezervace,pocetMistnosti,htmlSoubor);
         break;
     }
     case 3:{
-        vypisPodleCeny(mistnosti,pocetMistnosti);
+        vypisPodleCeny(mistnosti,pocetMistnosti,htmlSoubor);
         break;
     }
     case 4:{
-        vypisPodleMist(mistnosti,pocetMistnosti);
+        vypisPodleMist(mistnosti,pocetMistnosti,htmlSoubor);
         break;
     }
     case 5:{
-        vypisRezervace(rezervace,pocetRezervace);
+        vypisRezervace(rezervace,pocetRezervace,htmlSoubor);
         break;
     }
     default:{
@@ -206,7 +328,7 @@ else {
     }
  }
  if (error > 0){
-    return (askForAction(souborRezervace,mistnosti,rezervace,pocetMistnosti, pocetRezervace));
+    return (askForAction(souborRezervace,mistnosti,rezervace,pocetMistnosti, pocetRezervace,htmlSoubor));
 
  }
 return(0);
@@ -240,7 +362,8 @@ int startCom(ifstream &souborRezervace, Mistnosti mistnosti[], Rezervace rezerva
 
         }
     else {
-    askForAction(souborRezervace,mistnosti,rezervace, pocetMistnosti, pocetRezervace );
+    const char* stringToChar = htmlSoubor.c_str();
+    askForAction(souborRezervace,mistnosti,rezervace, pocetMistnosti, pocetRezervace,stringToChar);
     return(0);
     }
 
@@ -259,6 +382,7 @@ int main()
 
     Mistnosti* mistnosti = new Mistnosti[pocetMistnosti];
     Rezervace* rezervace = new Rezervace[pocetRezervace];
+    nactiRezervace (fileRezervace,rezervace);
     nactiMistnosti (fileMistnosti,mistnosti);
     cout << startCom(fileRezervace,mistnosti,rezervace,pocetMistnosti,pocetRezervace);
 
